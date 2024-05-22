973a416e8a31dfe745edfcb421027e64793b521e
oscare2307 <146147610+oscare2307@users.noreply.github.com>
2024-05-22 00:17:50 -0500

    Agregar archivos de proyecto.

---
+++ ConsumirApiRestV2/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ConsumirApiRestV2.Paginas;
+
+namespace ConsumirApiRestV2
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ConsultarProductoPage());
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ConsultarCategoriaPage());
+        }
+
+        private async void Button_Clicked_2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ConsultarUsuariosPage());
+        }
+    }
+
+}
